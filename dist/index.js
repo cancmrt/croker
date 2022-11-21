d0bcc8d23d90f94515858cf7135909bc6927d870
cancmrt <cancm91@gmail.com>
2022-11-21 20:48:15 +0300

    jobs some change

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BISTStaticValueCrawler_1 = require("./web-crawlers/bist/BISTStaticValueCrawler");
-var bistCrawler = new BISTStaticValueCrawler_1.BISTStaticValueCrawler();
-bistCrawler.Run();
-//# sourceMappingURL=index.js.map
